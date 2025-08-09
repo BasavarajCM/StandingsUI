6f07aadccdf5909c965702f10221a6a3a578ddaa
BasavarajCM <bmarapur@gmail.com>
2025-08-09 18:33:35 +0530

    refactored
    refactored

--- src/app/standings/standings-api.service.ts
+++ src/app/standings/standings-api.service.ts
@@ -6,39 +6,24 @@ import { catchError, tap } from 'rxjs/operators';
 @Injectable({ providedIn: 'root' })
 export class StandingsApiService {
   private apiUrl = 'http://localhost:8080/api/api/v1';
-  private cache: any = {};
 
   constructor(private http: HttpClient) {}
 
   getCountries(): Observable<any[]> {
-    if (this.cache.countries) return of(this.cache.countries);
-    return this.http.get<any[]>(`${this.apiUrl}/countries`).pipe(
-      tap(data => this.cache.countries = data),
-      catchError(() => of([]))
-    );
+     return this.http.get<any[]>(`${this.apiUrl}/countries`)
   }
 
   getLeagues(countryId: string): Observable<any[]> {
-    if (this.cache[`leagues_${countryId}`]) return of(this.cache[`leagues_${countryId}`]);
-    return this.http.get<any[]>(`${this.apiUrl}/leagues?countryId=${countryId}`).pipe(
-      tap(data => this.cache[`leagues_${countryId}`] = data),
-      catchError(() => of([]))
+    return this.http.get<any[]>(`${this.apiUrl}/leagues?countryId=${countryId}`
     );
   }
 
   getTeams(leagueId: string): Observable<any[]> {
-    if (this.cache[`teams_${leagueId}`]) return of(this.cache[`teams_${leagueId}`]);
-    return this.http.get<any[]>(`${this.apiUrl}/teams?leagueId=${leagueId}`).pipe(
-      tap(data => this.cache[`teams_${leagueId}`] = data),
-      catchError(() => of([]))
-    );
+   
+    return this.http.get<any[]>(`${this.apiUrl}/teams?leagueId=${leagueId}`)
   }
 
   getStandings(leagueId: string): Observable<any[]> {
-    if (this.cache[`standings_${leagueId}`]) return of(this.cache[`standings_${leagueId}`]);
-    return this.http.get<any[]>(`${this.apiUrl}/standings?leagueId=${leagueId}`).pipe(
-      tap(data => this.cache[`standings_${leagueId}`] = data),
-      catchError(() => of([]))
-    );
+    return this.http.get<any[]>(`${this.apiUrl}/standings?leagueId=${leagueId}`)
   }
 }
